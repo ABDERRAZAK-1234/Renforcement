d755977607a9ebd2efecee017a54d09f5efaf181
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-04-09 16:05:42 +0100

    J'ai terminer la semaine 2 pour UML et SQL

--- renforcement_2/semaine2/lundi.sql
+++ renforcement_2/semaine2/lundi.sql
@@ -22,3 +22,48 @@ SELECT * FROM employes ORDER BY salaire DESC;
 SELECT * FROM employes ORDER BY salaire DESC LIMIT 5;
 -- 6)-
 SELECT COUNT(*) FROM employes 
+
+-- *Challenge 4 — SQL : Le magasin en ligne
+
+-- create table articles
+CREATE TABLE articles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    prix DECIMAL(10,2), NOT NULL,
+    categorie VARCHAR(50) NOT NULL,
+    stock INT NOT NULL,
+    note_moyenne FLOAT NOT NULL
+);
+
+-- 1)-
+SELECT * FROM articles WHERE prix >= 20 AND prix <= 80
+-- 2)-
+SELECT * FROM articles WHERE nom LIKE '%Pro%';
+-- 3)-
+SELECT * FROM articles WHERE stock = 0;
+-- 4)-
+SELECT * FROM articles WHERE
+categorie IN ('Informatique', 'Téléphonie');
+-- 5)-
+SELECT * FROM articles
+WHERE prix > 50 AND note_moyenne >= 4;
+
+-- *Challenge 5 — SQL : Modifier les données
+
+-- 1)-
+UPDATE employes 
+SET salaire = salaire * 1.1 
+WHERE departement = 'Développement';
+-- 2)-
+UPDATE employes
+SET departement = 'Direction'
+WHERE id = 5;
+-- 3)-
+DELETE FROM employes
+WHERE date_embauche < '2015-01-01';
+-- 4)-
+SELECT 
+    AVG(salaire) AS salaire_moyen,
+    MAX(salaire) AS salaire_max,
+    COUNT(*) AS nb_employes
+FROM employes;
